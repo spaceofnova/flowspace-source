c6ed49a34376b97c3c7655e3e8eb884146c395ea
spaceofnova <mcrealms23@gmail.com>
2024-03-14 08:57:13 -0500

    Updated CSP to prepare for add-ons

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+            mode: 'auto', // 'hash' for production
+            directives: {
+                'default-src': ['self'],
+                'script-src': ['self', 'va.vercel-scripts.com'],
+                'style-src': ['self', 'nonce-%sveltekit.nonce%', 'fonts.googleapis.com'],
+                'connect-src': ['self', 'https://heroic-pipefish-74.clerk.accounts.dev', 'api.flowspace.app'],
+				"img-src": ["self", "data:", "img.clerk.com", 'files-novaspace.replit.app'],
+				'worker-src': ['self', 'blob:'],
+				"frame-src": ["self", "https://heroic-pipefish-74.clerk.accounts.dev", 'https://gsctbirdnthlooylyrsakgtnquyrydsz.replit.app/'],
+            }
+        }
 	}
 };
 
