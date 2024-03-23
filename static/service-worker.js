8eadd7487d41c6350529eca619855b12a8755633
spaceofnova <mcrealms23@gmail.com>
2024-03-23 18:00:44 -0500

    Updated Maskable Icon

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,7 +3,7 @@
 import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 precacheAndRoute(self.__WB_MANIFEST);
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v2.1';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
