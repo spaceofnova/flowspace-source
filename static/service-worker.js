5c52558a5c85ddc1d7001b5fd9e55e849603b7f0
spaceofnova <mcrealms23@gmail.com>
2024-03-23 15:57:58 -0500

    Service Worker Finally Fixed!!

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,7 +3,7 @@
 import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 precacheAndRoute(self.__WB_MANIFEST);
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1.95';
+const CACHE_NAME = 'static-cache-v2';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
